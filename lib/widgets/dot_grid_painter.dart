c72b5a46139dce6a008a691d68459f8e68d788ba
Chris Stayte <ctstayte@gmail.com>
2026-02-28 09:55:10 -0500

    Updated pattern sizing to make it thicker

--- lib/widgets/dot_grid_painter.dart
+++ lib/widgets/dot_grid_painter.dart
@@ -10,7 +10,7 @@ class DotGridPainter extends CustomPainter {
       ..style = PaintingStyle.fill;
 
     const spacing = 16.0;
-    const radius = 1.5;
+    const radius = 2.0;
 
     for (double x = spacing; x < size.width; x += spacing) {
       for (double y = spacing; y < size.height; y += spacing) {
